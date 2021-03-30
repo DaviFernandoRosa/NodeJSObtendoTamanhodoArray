43b0caac8db8a53b5c01086dddb7c11b3c4df6cb
Davi Fernando Rosa <davir0514@gmail.com>
2021-03-30 15:24:16 -0300

    Arquivo

---
+++ index.js
@@ -0,0 +1,18 @@
+var signos = [
+    "Áries",
+    "Touro",
+    "Gêmeos",
+    "Câncer",
+    "Leão",
+    "Virgem",
+    "Libra",
+    "Escorpião",
+    "Sagitário",
+    "Capricórnio",
+    "Aquário",
+    "Peixes"
+];
+
+console.log(signos.length); // 12
+
+//OBSERVAÇÃO VC CHAMA A VARIAVEL SEGUINDO COM .LENGTH
